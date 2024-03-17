ea6e5473168098d23edd3844b4ded62ddd794fcc
waszzz <139512912+waszzz@users.noreply.github.com>
2024-03-17 15:12:46 -0300

    dict.py

--- dict.py
+++ dict.py
@@ -1,1 +1,11 @@
+meme_dict = {}
+meme_dict["CRINGE"]="Algo excepcionalmente raro o embarazoso"
+meme_dict["LOL"]="Una respuesta común a algo gracioso"
+meme_dict["CREEPY"]="Algo aterrador, siniestro"
 
+for i in range(3):
+    word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    else:
+        print("¡Oops! Esa palabra aún no está registrada.")
